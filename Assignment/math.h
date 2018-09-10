0d446b6aea49d3233211bf1e5e9435534e01aa23
woodwardn9 <36169774+woodwardn9@users.noreply.github.com>
2018-09-10 18:50:13 -0400

    Update math.h
    Changed math.h to call on the int math function from my main.c file.

--- Assignment/math.h
+++ Assignment/math.h
@@ -1,9 +1,9 @@
 /*
  * math.h
  *
- *  Created on: Aug 22, 2017
- * 	Last Edited: Sept 7, 2018
- *      Author: Russell Trafford
+ *  Created on: Sept 10, 2018
+ * 	Last Edited: Sept 10, 2018
+ *  Author: Nick Woodward
  */
 
 /* Your assignment is to take the math function and implement at least the following functions:
@@ -23,7 +23,32 @@
 #ifndef MATH_H_
 #define MATH_H_
 
-//Part of your documentation should be listing the valid inputs and outputs for the functions you create.
 int math(int num1, int num2, char Operator);
+
+int main()
+{
+  //Takes 5 and 3 and adds them together using the add function int math
+math(5,3,'+');
+// Takes 5 and subtracts it by 3 using the subtract function int math
+math(5,3,'-');
+//Takes 6 and divides it by 3 using the divide function in int math
+math(6,3,'/');
+//Takes 10 and multiples it by 3 using the multiplication function in int math
+math(10,3,'*');
+//Takes 7 and divides it by 3 using the modulus function in int math
+math(7,3,'%');
+//Takes 5 and shifts it left 3 bits using the shift left function in int math
+math(5,3,'<');
+//Takes 50 and shifts it right 3 bits using the shift right function in int math
+math(50,3,'>');
+//Takes 4 and ANDs it together with 2 using the AND function in int math
+math(4,2,'&');
+//Takes 20 and ORs it together with 14 using the OR function in int math
+math(20,14,'|');
+//Takes 1 and XORs it together with 10 using the XOR function in int math
+math(1,10,'^');
+//Takes 1 and inverses its value using the inverese function in int math
+math(1,3,'~');
+}
 
 #endif /* MATH_H_ */
